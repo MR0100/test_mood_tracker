29aee0bc87adcec1a02e34dab8ad5dba3fb2df1d
MR0100 <mr.vaghasiya197@gmail.com>
2026-03-14 14:21:12 +0530

    fix(rename): change the namings of formatter

--- lib/core/extension/date_time_extensions.dart
+++ lib/core/extension/date_time_extensions.dart
@@ -1,10 +1,11 @@
 part of 'extensions.dart';
 
 extension DateTimeExtensions on DateTime {
-  String get formattedTimeDdMdYyyyHhMmAmPm => _formatTimeDDMMYYYYHHMMAMPM(this);
+  /// Convert the date time to DD/MM/YYYY HH:MM AM/PM format.
+  String get formattedTimeDdMdYyyyHhMmAmPm => _formatTimeDdMdYyyyHhMmAmPm(this);
 }
 
 // Formate time in DD/MM/YYYY HH:MM AM/PM
-String _formatTimeDDMMYYYYHHMMAMPM(DateTime timestamp) {
+String _formatTimeDdMdYyyyHhMmAmPm(DateTime timestamp) {
   return DateFormat('dd/MM/yyyy HH:mm a').format(timestamp);
 }
